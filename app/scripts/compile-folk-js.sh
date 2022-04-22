d5952cb05fb98bc233e5312753dd4e77c166cc8c
synycboom <synycboom@gmail.com>
2022-04-23 02:27:57 +0700

    fix: wrong sh compare syntax

--- app/scripts/compile-folk-js.sh
+++ app/scripts/compile-folk-js.sh
@@ -6,7 +6,7 @@ if [ -d node_modules/folks-finance-js-sdk/dist/v1 ]; then
   exit 0
 fi
 
-if [ "$1" == "alpine" ]; then
+if [ "$1" = "alpine" ]; then
   sed -i 's/src\/index\.ts/src\/\*\*\/\*\.ts/' node_modules/folks-finance-js-sdk/tsconfig.json
 else
   sed -i '' 's/src\/index\.ts/src\/\*\*\/\*\.ts/' node_modules/folks-finance-js-sdk/tsconfig.json
