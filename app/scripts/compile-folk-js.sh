1d2649d79ce4d905dfa1a7330a8afd17e778a37d
Tien <manotien@gmail.com>
2022-04-22 21:03:59 +0700

    fix: compile script for osx

--- app/scripts/compile-folk-js.sh
+++ app/scripts/compile-folk-js.sh
@@ -6,6 +6,6 @@ if [ -d node_modules/folks-finance-js-sdk/dist/v1 ]; then
   exit 0
 fi
 
-sed -i 's/src\/index\.ts/src\/\*\*\/\*\.ts/' node_modules/folks-finance-js-sdk/tsconfig.json
+sed -i '' 's/src\/index\.ts/src\/\*\*\/\*\.ts/' node_modules/folks-finance-js-sdk/tsconfig.json
 
 cd node_modules/folks-finance-js-sdk && npm run build
