43ee4f0de42d5abe55613640b1f976d06d2406b0
synycboom <synycboom@gmail.com>
2022-04-23 02:14:07 +0700

    fix: alpine build problem with sed

--- app/scripts/compile-folk-js.sh
+++ app/scripts/compile-folk-js.sh
@@ -6,6 +6,10 @@ if [ -d node_modules/folks-finance-js-sdk/dist/v1 ]; then
   exit 0
 fi
 
-sed -i '' 's/src\/index\.ts/src\/\*\*\/\*\.ts/' node_modules/folks-finance-js-sdk/tsconfig.json
+if [ "$1" == "alpine" ]; then
+  sed -i 's/src\/index\.ts/src\/\*\*\/\*\.ts/' node_modules/folks-finance-js-sdk/tsconfig.json
+else
+  sed -i '' 's/src\/index\.ts/src\/\*\*\/\*\.ts/' node_modules/folks-finance-js-sdk/tsconfig.json
+fi
 
 cd node_modules/folks-finance-js-sdk && npm run build
